2cdc9d2c23c197c7bc1566b85e270d23c4025db4
David Ikin <davidikin45@gmail.com>
2018-10-01 15:49:25 +0300

    Hangfire

---
+++ ConsoleApp/Hangfire/LogEverythingAttribute.cs
@@ -0,0 +1,26 @@
+﻿using Hangfire.Common;
+using Hangfire.Logging;
+using Hangfire.Server;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp.Hangfire
+{
+    //http://docs.hangfire.io/en/latest/extensibility/using-job-filters.html
+    public class LogEverythingAttribute : JobFilterAttribute,
+    IServerFilter
+    {
+
+
+        public void OnPerforming(PerformingContext context)
+        {
+            //Logger.InfoFormat("Starting to perform job `{0}`", context.BackgroundJob.Id);
+        }
+
+        public void OnPerformed(PerformedContext context)
+        {
+            //Logger.InfoFormat("Job `{0}` has been performed", context.BackgroundJob.Id);
+        }
+    }
+}
---
+++ ConsoleApp/ProgramHangfireServer.cs
@@ -0,0 +1,33 @@
+﻿using ConsoleApp.Hangfire;
+using Hangfire;
+using Hangfire.MemoryStorage;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp
+{
+    public class ProgramHangfireServer
+    {
+        public static void Main2(string[] args)
+        {
+
+            //http://docs.hangfire.io/en/latest/extensibility/using-job-filters.html
+            GlobalJobFilters.Filters.Add(new LogEverythingAttribute());
+
+            GlobalConfiguration.Configuration.UseMemoryStorage();
+
+            //http://docs.hangfire.io/en/latest/background-processing/configuring-queues.html
+            var options = new BackgroundJobServerOptions
+            {
+                Queues = new string[]{"default"}
+            };
+
+            using (var server = new BackgroundJobServer(options))
+            {
+                Console.WriteLine("Hangfire Server started. Press any key to exit...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
